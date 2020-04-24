e28df4bc8860970f995665e4081f099773d07fdd
happyjarban <happyjarban@gmail.com>
2020-04-24 16:43:28 +0900

    update REAMD.MD

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# blog
-blog with DRF(Django REST API) and vue.js
+# Blog( API server )
+ This repository is an example of a restapi server with SPA
+
+
+## STACK
+- DRF(Django REST API) 
+    - jwt
+    - markdownx
+    - cors-header
+    - djangorestframework
+- Vue.js
+    - vuex
+    - vue-router
+    - bootstrap-vue
+    - marked
+    - vue-disqus
+
+
+
+
+### Do you want to move on to the next step?
+
+ - [realworld](https://github.com/gothinkster/vue-realworld-example-app)
+ - [snipcart](https://github.com/snipcart/vue-blog-demo)
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,27 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 import router from "./routes/index.js";
 import store from "./store/index.js";
-import VueDisqus from 'vue-disqus';
+import VueDisqus from "vue-disqus";
 
-
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// BootStrap
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
 Vue.use(VueDisqus);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router, store,
-  render: h => h(App),
-}).$mount('#app')
+  beforeCreate() {
+    let token = sessionStorage.getItem("access_token");
+    if (token) {
+      this.$store.dispatch("getMemberInfo");
+    }
+  },
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
